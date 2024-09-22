82266f587c07cd00a17eb4c0a6e83143554ea1af
duama20180 <accforwork20180@gmail.com>
2024-09-22 23:40:02 +0300

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const tooltip = document.getElementById("tooltip");
+
+        const troitsyaDate = new Date("2025-06-08");
+        const today = new Date();
+
+        const diffTime = Math.abs(troitsyaDate - today);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+ 
+        tooltip.textContent = `До Зелених cвят залишилось ${diffDays} днів`;
+    });
