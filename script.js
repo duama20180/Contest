d8a46181426bef9a76a35fd2c5e187226d7e6ba4
duama20180 <accforwork20180@gmail.com>
2024-10-09 10:03:38 +0300

    minor changes

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tooltip = document.getElementById("tooltip");
 
-    document.addEventListener("DOMContentLoaded", function() {
-        const tooltip = document.getElementById("tooltip");
+    const troitsyaDate = new Date("2025-06-08");
+    const today = new Date();
 
-        const troitsyaDate = new Date("2025-06-08");
-        const today = new Date();
+    const diffTime = Math.abs(troitsyaDate - today);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-        const diffTime = Math.abs(troitsyaDate - today);
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    tooltip.textContent = `До Зелених cвят залишилось ${diffDays} днів`;
 
- 
-        tooltip.textContent = `До Зелених cвят залишилось ${diffDays} днів`;
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    menuToggle.addEventListener("click", function() {
+        menu.classList.toggle("open");
     });
+});
+
